74bd1f704ce1076215901df2ed3ef2e5616c315e
Ludwig Huang <0711feiyu@gmail.com>
2023-04-14 10:32:44 +0800

    feat: implement ethernet_out

--- src/ethernet.c
+++ src/ethernet.c
@@ -8,9 +8,7 @@
  *
  * @param buf 要处理的数据包
  */
-void ethernet_in(buf_t *buf) {
-  // TO-DO
-}
+void ethernet_in(buf_t *buf) {}
 /**
  * @brief 处理一个要发送的数据包
  *
@@ -19,7 +17,26 @@ void ethernet_in(buf_t *buf) {
  * @param protocol 上层协议
  */
 void ethernet_out(buf_t *buf, const uint8_t *mac, net_protocol_t protocol) {
-  // TO-DO
+  // 1. padding if less than 46
+  if (buf->len < ETHERNET_MIN_TRANSPORT_UNIT) {
+    buf_add_padding(buf, ETHERNET_MIN_TRANSPORT_UNIT - buf->len);
+  }
+
+  // 2. add ethernet header
+  buf_add_header(buf, sizeof(ether_hdr_t)); // allocate header & move data ptr
+  ether_hdr_t *hdr = (ether_hdr_t *)buf->data;
+
+  // 3. destination MAC address
+  memcpy(hdr->dst, mac, NET_MAC_LEN);
+
+  // 4. source MAC address
+  memcpy(hdr->src, net_if_mac, NET_MAC_LEN);
+
+  // 5. protocol type
+  hdr->protocol16 = swap16(protocol);
+
+  // 6. send data frame to driver
+  driver_send(buf);
 }
 /**
  * @brief 初始化以太网协议
